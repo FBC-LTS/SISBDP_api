85726ab2e0433e57d1ef0b0c196ad6b319750ba6
Lucimar Barros <lucimar7324@hotmail.com>
2024-05-01 03:13:00 -0300

    Update db_barbearia.sql
    Foi atualizada as tabelas e adicionada outras tabelas!

--- BD/db_barbearia.sql
+++ BD/db_barbearia.sql
@@ -3,79 +3,101 @@ use db_barbearia;
 
 CREATE TABLE produtos
  (
-    id int unsigned not null auto_increment, 
-    nome varchar(50) not null,
-    categoria varchar(30) not null,
-    quantidade int not null,
-    preco DECIMAL(10,2),
-    PRIMARY KEY (id)
+    id_produto int unsigned not null auto_increment, 
+    nome_produto varchar(50) not null,
+    categoria_produto varchar(50) not null,
+    quantidade_produto int not null,
+    preco_produto DECIMAL(10,2),
+    PRIMARY KEY (id_produto)
+);
+
+CREATE TABLE itens_produtos
+(
+    FOREIGN KEY (fk_id_produto) REFERENCES produtos(id_produto),
+    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
 );
 
 CREATE TABLE clientes 
 (
-    id int unsigned not null auto_increment,
-    nascimento date not null,
-    idade varchar(50) not null,
-    email varchar(255) not null,
-    telefone varchar(20) not null,
-    PRIMARY KEY (id)
+    id_cliente int unsigned not null auto_increment,
+    nascimento_cliente date not null,
+    email_cliente varchar(255) not null,
+    telefone_cliente varchar(50) not null,
+    observacao_cliente text,
+    PRIMARY KEY (id_cliente)
 );
 
 CREATE TABLE usuarios 
 (
-    id int unsigned not null auto_increment,
-    nome varchar(50) not null,
-    telefone int not null,
-    email varchar(50) not null,
-    senha varchar(50) notnull,
-    PRIMARY KEY (id)
+    id_usuario int unsigned not null auto_increment,
+    nome_usuario varchar(50) not null,
+    telefone_usuario int not null,
+    email_usuario varchar(255) not null,
+    senha_usuario varchar(50) not null,
+    PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE vendas 
 (
-    id int unsigned not null auto_increment PRIMARY KEY,
-    total decimal not null,
-    data date not null,
-    hora time not null,
-    desconto decimal not null,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    id_venda int unsigned not null auto_increment,
+    total_venda decimal not null,
+    data_venda date not null,
+    hora_venda time not null,
+    desconto_venda decimal not null,
+    PRIMARY KEY (id_venda),
+    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente),
+    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario)
 );
 
-CREATE TABLE servico
+CREATE TABLE servicos
  (
-    id int not null auto_increment,
-    nome varchar(30) not null,
-    preco decimal(10,2) not null,
-    observacao varchar(50) not null,
-    PRIMARY KEY (id)
+    id_servico int not null auto_increment,
+    nome_servico varchar(50) not null,
+    preco_servico decimal(10,2) not null,
+    observacao_servico text,
+    PRIMARY KEY (id_servico)
 );
+
+CREATE TABLE itens_servicos
+(
+    FOREIGN KEY (fk_id_servico) REFERENCES servicos(id_servico),
+    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
+);
+
 
 CREATE TABLE agendamentos
 (
-    id int not null auto_increment,
-    Data DATE not null,
-    Horario TIME not null,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    id_agendamento int not null auto_increment,
+    data_agendamento DATE not null,
+    Horario_agendamento TIME not null,
+    PRIMARY KEY (id_agendamento),
+    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente)
     );
 
-CREATE TABLE cortes 
+CREATE TABLE galeria 
 (
-    id int unsigned not null auto_increment,
-    nome varchar(30) not null,
-    dados_imagem longblob,
-    descricao varchar(50) not null,
-    url varchar(255),
-    PRIMARY KEY (id)
+    id_corte int unsigned not null auto_increment,
+    nome_corte varchar(50) not null,
+    descricao_corte varchar(50) not null,
+    url text,
+    PRIMARY KEY (id_corte),
+    FOREIGN KEY (fk_id_imagem) REFERENCES imagens(id_imagem)
+);
+
+CREATE TABLE imagens
+(
+    id_imagem int unsigned not null auto_increment,
+    imagem blob,
+    PRIMARY KEY (id_imagem)
 );
 
 CREATE TABLE gastos
 (
-    id int unsigned not null auto_increment,
-    nome varchar(30) not null,
-    preco decimal not null,
-    descricao varchar(50) not null,
-    data date,
-    PRIMARY KEY (id)
+    id_gasto int unsigned not null auto_increment,
+    nome_gasto varchar(50) not null,
+    preco_gasto decimal not null,
+    descricao_gasto text,
+    data_gasto date,
+    PRIMARY KEY (id_gasto)
 );
