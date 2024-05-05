42b75ad91eefa5c8986e54dacef91b055cfdeb04
FBC-LTS <fabricioalcc@gmail.com>
2024-05-04 23:02:33 -0300

    Update db_barbearia.sql
    fix: padronizando e ajustando tipos.
    info: testado no railway

--- BD/db_barbearia.sql
+++ BD/db_barbearia.sql
@@ -2,102 +2,108 @@ create database db_barbearia;
 use db_barbearia;
 
 CREATE TABLE produtos
- (
-    id_produto int unsigned not null auto_increment, 
-    nome_produto varchar(50) not null,
-    categoria_produto varchar(50) not null,
-    quantidade_produto int not null,
-    preco_produto DECIMAL(10,2),
-    PRIMARY KEY (id_produto)
-);
-
-CREATE TABLE itens_produtos
 (
-    FOREIGN KEY (fk_id_produto) REFERENCES produtos(id_produto),
-    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
+    id_produto bigint unsigned not null auto_increment, 
+    nome_produto text not null,
+    categoria_produto text not null,
+    quantidade_produto int not null,
+    preco_produto DECIMAL(10,2) not null,
+    PRIMARY KEY (id_produto)
 );
 
 CREATE TABLE clientes 
 (
-    id_cliente int unsigned not null auto_increment,
+    id_cliente bigint unsigned not null auto_increment,
     nascimento_cliente date not null,
-    email_cliente varchar(255) not null,
-    telefone_cliente varchar(50) not null,
+    email_cliente text not null,
+    telefone_cliente text not null,
     observacao_cliente text,
     PRIMARY KEY (id_cliente)
 );
 
 CREATE TABLE usuarios 
 (
-    id_usuario int unsigned not null auto_increment,
-    nome_usuario varchar(50) not null,
-    telefone_usuario int not null,
-    email_usuario varchar(255) not null,
-    senha_usuario varchar(50) not null,
+    id_usuario bigint unsigned not null auto_increment,
+    nome_usuario text not null,
+    telefone_usuario text not null,
+    email_usuario text not null,
+    senha_usuario text not null,
     PRIMARY KEY (id_usuario)
-);
-
-CREATE TABLE vendas 
-(
-    id_venda int unsigned not null auto_increment,
-    total_venda decimal not null,
-    data_venda date not null,
-    hora_venda time not null,
-    desconto_venda decimal not null,
-    PRIMARY KEY (id_venda),
-    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente),
-    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario)
 );
 
 CREATE TABLE servicos
  (
-    id_servico int not null auto_increment,
-    nome_servico varchar(50) not null,
+    id_servico bigint unsigned not null auto_increment,
+    nome_servico text not null,
     preco_servico decimal(10,2) not null,
     observacao_servico text,
     PRIMARY KEY (id_servico)
 );
 
-CREATE TABLE itens_servicos
-(
-    FOREIGN KEY (fk_id_servico) REFERENCES servicos(id_servico),
-    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
-);
-
-
-CREATE TABLE agendamentos
-(
-    id_agendamento int not null auto_increment,
-    data_agendamento DATE not null,
-    Horario_agendamento TIME not null,
-    PRIMARY KEY (id_agendamento),
-    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente)
-    );
-
-CREATE TABLE galeria 
-(
-    id_corte int unsigned not null auto_increment,
-    nome_corte varchar(50) not null,
-    descricao_corte varchar(50) not null,
-    url text,
-    PRIMARY KEY (id_corte),
-    FOREIGN KEY (fk_id_imagem) REFERENCES imagens(id_imagem)
-);
-
 CREATE TABLE imagens
 (
-    id_imagem int unsigned not null auto_increment,
+    id_imagem bigint unsigned not null auto_increment,
     imagem blob,
+    url_imagem text,
     PRIMARY KEY (id_imagem)
 );
 
 CREATE TABLE gastos
 (
-    id_gasto int unsigned not null auto_increment,
-    nome_gasto varchar(50) not null,
+    id_gasto bigint unsigned not null auto_increment,
+    nome_gasto text not null,
     preco_gasto decimal not null,
     descricao_gasto text,
-    data_gasto date,
+    data_gasto datetime,
     PRIMARY KEY (id_gasto)
+);
+
+CREATE TABLE galeria 
+(
+    id_corte bigint unsigned not null auto_increment,
+    nome_corte text not null,
+    descricao_corte text,
+    fk_id_imagem bigint unsigned,
+    PRIMARY KEY (id_corte),
+    FOREIGN KEY (fk_id_imagem) REFERENCES imagens(id_imagem)
+);
+
+CREATE TABLE agendamentos
+(
+    id_agendamento bigint unsigned not null auto_increment,
+    data_agendamento datetime not null,
+    fk_id_cliente bigint unsigned,
+    fk_id_usuario bigint unsigned, 
+    PRIMARY KEY (id_agendamento),
+    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE itens_servicos
+(
+    fk_id_servico bigint unsigned,
+    fk_id_venda bigint unsigned,
+    FOREIGN KEY (fk_id_servico) REFERENCES servicos(id_servico),
+    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
+);
+
+CREATE TABLE vendas
+(
+    id_venda bigint unsigned not null auto_increment,
+    total_venda decimal not null,
+    data_venda datetime not null,
+    desconto_venda decimal,
+    fk_id_cliente bigint unsigned,
+    fk_id_usuario bigint unsigned, 
+    PRIMARY KEY (id_venda),
+    FOREIGN KEY (fk_id_cliente) REFERENCES clientes(id_cliente),
+    FOREIGN KEY (fk_id_usuario) REFERENCES usuarios(id_usuario)
+);
+
+CREATE TABLE itens_produtos
+(
+    fk_id_produto bigint unsigned,
+    fk_id_venda bigint unsigned,
+    FOREIGN KEY (fk_id_produto) REFERENCES produtos(id_produto),
+    FOREIGN KEY (fk_id_venda) REFERENCES vendas(id_venda)
 );
