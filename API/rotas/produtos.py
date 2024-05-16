57a0d31bd54cf2d44e1b6ca6a8aab168106e1454
FBC-LTS <fabricioalcc@gmail.com>
2024-05-16 18:21:06 -0300

    Update produtos.py
    feat: Conexão com o BD e resposta na rota produtos.json

--- API/rotas/produtos.py
+++ API/rotas/produtos.py
@@ -1,12 +1,28 @@
 from fastapi import APIRouter
+from fastapi import Response
+from auth import Conect
+import json
 
 router = APIRouter()
 
+
+
 @router.get("/produtos.json")
 async def get_produtos():
+    dados = Conect()
+    dados.conectar()
+    val, produtos = dados.get_produtos()
     #conectar bd puxar produtos e serviços
-    return {
-        "total_itens": 0,
-        "Produtos": {}
-        }
+    if val:
+        res = produtos.to_dict('records')
+
+    else:
+        return Response(
+            "ERRO AO PEGAR DADOS", status_code=204
+        )
+
+    return Response(
+        f'"total_itens": {len(res)},"Produtos": {res}'
+    )
+        
 
